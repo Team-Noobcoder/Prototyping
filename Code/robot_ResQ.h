ccadff7463d069743353198d5c19286e48b0dc36
VillaKimi <muhammad-amirul-hakimi.bin-zaprunnizam@stud.hshl.de>
2021-06-17 16:06:24 +0200

    Update robot_ResQ.h

--- Code/robot_ResQ.h
+++ Code/robot_ResQ.h
@@ -1,14 +1,23 @@
 #pragma once
 #ifndef ROBOT_H
 #define ROBOT_H
+#include <stdbool.h>
 
-static int driving_mode = 0;
-static char coming_from = 'O';
 static int status = 0;
+static int Robot = 0;
+static int Target = 0;
+
+/*
+int bal = sizeof(queue) / sizeof(int);// size queue
+int ck = sizeof(previous) / sizeof(int);// size previous
+int kl = sizeof(path) / sizeof(int); //size path
+int am = sizeof(repath) / sizeof(int);
+*/
+
 // we always start on an empty space
 // Function declaration
 int move(char* world);
 
 #endif // ROBOT_H#pragma once
 #pragma once
-
+#pragma once
