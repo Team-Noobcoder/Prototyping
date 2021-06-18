a27e6767a1c24e569d17a9eff9f85b90eb75efdf
VillaKimi <muhammad-amirul-hakimi.bin-zaprunnizam@stud.hshl.de>
2021-06-18 02:24:41 +0200

    Update robot_ResQ.c

--- Code/robot_ResQ.c
+++ Code/robot_ResQ.c
@@ -1,4 +1,3 @@
-
 #include "robot_ResQ.h"
 
 static int queue[200];
@@ -6,14 +5,15 @@ static int visited[200];
 static int previous[200];
 static int current_position;
 static int direction[4] = { 1,-1,21,-21 };
-static int y = 1;
-static int c = 0;
+static int y;
+static int c;
 static int path[200];
 static int repath[200];
 static int p = 0;
 static int b = 0;
 static int construct;
-
+static int position_target;
+static int new_current_position;
 
 
 int move(char* world)
@@ -21,18 +21,24 @@ int move(char* world)
 	// find R location
 	for (Robot = 0; world[Robot] != 'R'; Robot++)
 	{
+		current_position = Robot;
 
 	}
 	// find T location
 	for (Target = 0; world[Target] != 'T'; Target++)
 	{
+		position_target = Target;
+
 
 	}
 
 
+	printf("%d\n", current_position);
+	printf("%d\n", position_target);
 
-	Robot = current_position;
 	queue[0] = current_position;
+	printf("%d\n", queue[0]);
+
 
 	for (int v = 0; v < 200; v++) // v for visited)
 	{
@@ -43,20 +49,31 @@ int move(char* world)
 	visited[current_position] = 1;
 
 
+	for (int prv = 0; prv < 200; prv++) // prv for previous
+	{
+		previous[prv] = 0;
 
+	}
 
 
 
 
 	// loop to check neighbour and inside array queue
 
-	int y = 1;
-	while (queue[y] != Target)
+
+
+	int* pointer;
+	pointer = &queue[0];
+	c = 0;
+
+	for (int y = 0; queue[y] != position_target; c++)
 
 	{
+		current_position = *pointer;
 
 		//queue[c] = current_position; // c for counter to check array neighbour
-		current_position = queue[c];
+		  //current_position = queue[c] ;
+
 		for (int d = 0; d < 4; d++) // d for direction
 
 		{
@@ -66,20 +83,33 @@ int move(char* world)
 
 			}
 
-			else if (visited[current_position] == 1)
+			else if (world[current_position + direction[d]] < 0 || world[current_position + direction[d]]>200)
 
 			{
 
 
 			}
+
+
+
+			else if (visited[current_position + direction[d]] == 1)
+
+			{
+
+
+			}
+
 			else if (world[current_position + direction[d]] == 'T')
 			{
 
-				queue[y] = current_position + direction[d];
-				d = 5;
-				y++;
+				queue[y + 1] = current_position + direction[d];
+
+
 				visited[current_position + direction[d]] = 1;
 				previous[current_position + direction[d]] = current_position;
+				//current_position = current_position + direction[d];
+				y++;
+				break;
 			}
 
 
@@ -87,28 +117,32 @@ int move(char* world)
 			{
 				//current_position + direction[d] = queue[y];
 
-				queue[y] = current_position + direction[d];
-				y++;
+				queue[y + 1] = current_position + direction[d];
+
 				visited[current_position + direction[d]] = 1;
 				previous[current_position + direction[d]] = current_position;
+				//current_position = current_position + direction[d];
+				printf("%d ", queue[y + 1]);
+				y++;
+
 
 			}
 		}
 
 
-		c++;
+		pointer++;
 
 	}
 
 
 	// construct path from target to robot
 
-	for (construct = Target; previous[construct] != 'NULL'; construct = previous[Target])
+	for (construct = position_target; previous[construct] != 0; construct = previous[position_target])
 	{
 
 		path[p] = construct;
 		p++;
-
+		printf("%d", path[p]);
 	}
 
 	int r = p;
