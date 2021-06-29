180e174a619d6b7d6ed4fc8810ed623cfbd5860c
iqbalfauzii <muhammad-iqbal.bin-mohd-fauzi@stud.hshl.de>
2021-06-29 15:28:10 +0200

    Create robot_ResQtask4.h

---
+++ Code/Task 4/robot_ResQtask4.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef ROBOT_H
+#define ROBOT_H
+#include <stdbool.h>
+
+
+int find_path(int raw_count, int r_x, int r_y, int t_x, int t_y, char* world);
+int move(char* world, int map_id);
+
+
+
+static int status;
+static int Robot;
+static int Target;
+static int t_water;
+static int current_position;
+
+
+
+
+// we always start on an empty space
+// Function declaration
+
+
+#endif // ROBOT_H#pragma once
+#pragma once
+#pragma once
+
